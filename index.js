a18e768e6317651df919baf442a20edc24df3b07
François <yyperite@hotmail.fr>
2019-09-14 11:33:26 +0200

    update pendu

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ let start = document.getElementById("start");
 let reponse;
 let mots = ["adoption", "amitié", "cabane", "chapeaux","citrouille","corbeau","dictionnaire","figurine","indépendence","journal","lunettes","mercantile","pasteur","ruisseau","tableau","vélocipède"];
 let count = 0;
-let lettreTrouve = 0;
 let stockUnderscore = [];
+
+function drawCanvas() {
+    count = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+  }
 function randomMot() {
     
+    drawCanvas();
     reponse = mots[Math.floor(Math.random() * mots.length)];
     stock = reponse.split('')
     console.log(stock);
@@ -37,12 +43,12 @@ function analyz(){
             stockUnderscore[i] = stock[i];
             ecran.textContent = stockUnderscore.join("");
             trouve = true;
-            lettreTrouve++;
         }
 
     }
     if(ecran.textContent == reponse){
         alert("Win!");
+        
     }
     console.log(stockUnderscore)
     
@@ -81,92 +87,4 @@ function analyz(){
     ctx.stroke();
 }
 submit.addEventListener("click", analyz);
-        
-        
-
-/*function analyz() {
-    ctx.beginPath();
-    ctx.strokeStyle = "#000";
-    ctx.lineWidth = 3;
-
-    if (ecran.textContent == reponse) {
-
-        alert("Gagner !");
-
-    }else if (count == 1) {
-        ctx.moveTo(50, 300);//sol
-        ctx.lineTo(250, 300);//sol
-        console.log("coucou");
-
-    } else if (count == 2) {
-        ctx.moveTo(150, 300);//poutre
-        ctx.lineTo(150, 50);//poutre
-        ctx.lineTo(300, 50);//poutre
-        ctx.lineTo(300, 80);//poutre
-        console.log("coucou2");
-    } else if (count == 3) {
-        ctx.moveTo(320, 100);
-        ctx.arc(300, 100, 20, 0, Math.PI * 2,
-            true);//tete
-        console.log("coucou2");
-    } else if (count == 4) {
-        ctx.moveTo(300, 120);//corp
-        ctx.lineTo(300, 200);//corp
-        console.log("coucou2");
-    } else if (count == 5) {
-        ctx.moveTo(350, 150);//bras
-        ctx.lineTo(250, 150);//bras
-        console.log("coucou2");
-    } else if (count == 6) {
-        ctx.moveTo(340, 250);//jambes
-        ctx.lineTo(300, 200);//jambes
-        ctx.lineTo(260, 250);//jambes
-        console.log("coucou2");
-    } else if (count == 7) {
-
-        alert("death");
-    }
-    ctx.stroke();
-
-    /*if (input.value == reponse.charAt(0)) {
-
-        ecran.textContent = ecran.textContent.replace(ecran.textContent.charAt(0), "-");
-
-    }if(input.value == reponse.charAt(1)){
-
-        ecran.textContent = ecran.textContent.replace(ecran.textContent.charAt(1), "-");
-    }
-    else {
-
-        console.log(count++);
-        
-    }
-}*/
-
-//submit.addEventListener("click", analyz);
-
-
-
-
-
-
-
-
-
-//ctx.fillRect(10,20,100,200);
-
-//ctx.strokeRect(300,300,200,200);
-
-// ctx.beginPath();
-// ctx.moveTo(100,200);
-// ctx.lineTo(100,300);
-// ctx.moveTo(100,200);
-// ctx.lineTo(200,200);
-// ctx.moveTo(100,250);
-// ctx.lineTo(150,250);
-//ctx.stroke();
-
-
-
-
-
+ 
